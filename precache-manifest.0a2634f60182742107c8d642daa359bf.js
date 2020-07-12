d22597ffa6c5429f00c709bdbfc23c469ffbce0f
Koushik <koushik.ch@pitsolutions.com>
2020-07-13 00:34:20 +0530

    Updates

---
+++ precache-manifest.0a2634f60182742107c8d642daa359bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2a20256341a7cdfdd3afa5d71928d9",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "b38b042248aa0b1abc72",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbecef1f07b827a82dbf",
+    "url": "/react-test/static/js/2.89e7a954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.89e7a954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38b042248aa0b1abc72",
+    "url": "/react-test/static/js/main.d32605b9.chunk.js"
+  },
+  {
+    "revision": "9889ddb3af4f0a50bdbe",
+    "url": "/react-test/static/js/runtime-main.d36f5469.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
